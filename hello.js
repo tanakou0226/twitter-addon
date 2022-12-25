39a337592ec514e04b60be6ca5effafc3c91d43a
tanakou <tanakou.0226@gmail.com>
2022-12-26 04:03:14 +0900

    滞在時間の表示

--- hello.js
+++ hello.js
@@ -1,20 +1,28 @@
-console.log("Content script is loaded")
+var loadDate = new Date();
+var steyTime = 0
 
-function loadFinished(event){
-    console.log(event);
-}
+var flag = false
 
-window.addEventListener('load', getHeader);
-
- function getHeader ()  {
+function showTime ()  {
     var HeaderNode = document.getElementsByClassName("css-1dbjc4n r-1awozwy r-1r5su4o r-e7q0ms")[0]
     if (!HeaderNode) return false;
     var timeDiv = document.createElement("div");
-    timeDiv.innerHTML = "<div>now time</div>"
+    timeDiv.innerHTML = `<div id="stey">滞在時間：${steyTime}</div>`
     HeaderNode.appendChild(timeDiv)
+    flag = true
     return true
 }
-
 var interval = setInterval(function () {
-    if(getHeader()) clearInterval(interval)
+    if(showTime()) clearInterval(interval)
 }, 1000)
+
+function cns(){
+    steyTime = new Date() - loadDate
+    if (flag) {
+        var timeNode = document.getElementById("stey")
+        var time = parseInt(steyTime / 1000, 10)
+        timeNode.innerHTML = `<div id="stey">滞在時間：${time}秒</div>`
+    }
+}
+
+setInterval(cns, 1000)
